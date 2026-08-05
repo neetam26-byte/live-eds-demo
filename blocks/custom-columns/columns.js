94cd1c68ae47f20ce1b948554aea3aca3150a752
Neeta Marne <neetam26@gmail.com>
2026-08-05 13:17:28 +0530

    Added custom columns

---
+++ blocks/custom-columns/columns.js
@@ -0,0 +1,13 @@
+export default function decorate(block) {
+  const rows = [...block.children];
+
+  rows.forEach((row) => {
+    const columns = [...row.children];
+
+    row.classList.add(`columns-${columns.length}`);
+
+    columns.forEach((column) => {
+      column.classList.add('column');
+    });
+  });
+}
