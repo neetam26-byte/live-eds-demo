e8657a0d2897cafb67ffacc0f91f4368295099d3
Neeta Marne <neetam26@gmail.com>
2026-08-05 14:58:15 +0530

    updated custom columns

--- blocks/custom-columns/custom-columns.js
+++ blocks/custom-columns/custom-columns.js
@@ -2,9 +2,9 @@ export default function decorate(block) {
   const rows = [...block.children];
 
   rows.forEach((row) => {
-    const columns = [...row.children];
+    const custom-columns = [...row.children];
 
-    row.classList.add(`columns-${columns.length}`);
+    row.classList.add(`columns-${custom-columns.length}`);
 
     columns.forEach((column) => {
       column.classList.add('custom-column');
